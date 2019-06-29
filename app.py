d9830cd5467729e551fa5a5f5c7a607c13ecc0ce
mtalimanchuk <mtalimanchuk@gmail.com>
2019-06-29 19:53:59 +0300

    Uncommented db operations

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ class Config(object):
 
 config = Config()
 app.config.from_object(config)
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 """ORM"""
 LIST_ENTITY_NAME = 'List'
@@ -57,52 +57,52 @@ EXERCISE_BACK_REFERENCE = 'exercise'
 TASKS_ENTITY_NAME = 'Task'
 
 
-# class Sentence(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-#     lang = db.Column(db.String(3), index=True, nullable=False)
-#     text = db.Column(db.Text(42940000), nullable=False)
-#     list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=True)
-#     exercise_id = db.Column(db.Integer, db.ForeignKey(EXERCISE_REFERENCE), nullable=True)
-#
-#
-# class List(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-#     name = db.Column(db.String(300), nullable=False)
-#
-#     sentences = db.relationship(SENTENCE_ENTITY_NAME, backref=LIST_BACK_REFERENCE, lazy=True)
-#
-#
-# class Exercise(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-#     student_url = db.Column(db.String(30), unique=True, nullable=False)
-#     list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=True)
-#     expiration_datetime = db.Column(db.TIMESTAMP, nullable=False)
-#
-#     sentences = db.relationship(EXERCISE_ENTITY_NAME, backref=SENTENCE_BACK_REFERENCE, lazy=True)
-#     tasks = db.relationship(TASKS_ENTITY_NAME, backref=EXERCISE_BACK_REFERENCE, lazy=False)
-#
-#
-# class Task(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-#     exercise_id = db.Column(db.Integer, db.ForeignKey(EXERCISE_REFERENCE), nullable=False, unique=True)
-#     sentence_id = db.Column(db.Integer, db.ForeignKey(SENTENCE_REFERENCE), nullable=False)
-#     correct_answer = db.Column(db.String(50), nullable=False)
-#     task_input = db.Column(db.String(50), nullable=True)
-#     is_completed = db.Column(db.Boolean, default=False)
-#     failed_attempts = db.Column(db.Integer, nullable=False, default=0)
-#
-#
-# class SentenceToList(db.Model):
-#     sentence_id = db.Column(db.Integer, db.ForeignKey(SENTENCE_REFERENCE), nullable=False)
-#     list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=False)
-#
-#     __table_args__ = (
-#         db.PrimaryKeyConstraint(sentence_id, list_id),
-#         {}
-#     )
-#
-#
-# migrate = Migrate(app, db)
+class Sentence(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    lang = db.Column(db.String(3), index=True, nullable=False)
+    text = db.Column(db.Text(42940000), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey(EXERCISE_REFERENCE), nullable=True)
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.String(300), nullable=False)
+
+    sentences = db.relationship(SENTENCE_ENTITY_NAME, backref=LIST_BACK_REFERENCE, lazy=True)
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    student_url = db.Column(db.String(30), unique=True, nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=True)
+    expiration_datetime = db.Column(db.TIMESTAMP, nullable=False)
+
+    sentences = db.relationship(EXERCISE_ENTITY_NAME, backref=SENTENCE_BACK_REFERENCE, lazy=True)
+    tasks = db.relationship(TASKS_ENTITY_NAME, backref=EXERCISE_BACK_REFERENCE, lazy=False)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey(EXERCISE_REFERENCE), nullable=False, unique=True)
+    sentence_id = db.Column(db.Integer, db.ForeignKey(SENTENCE_REFERENCE), nullable=False)
+    correct_answer = db.Column(db.String(50), nullable=False)
+    task_input = db.Column(db.String(50), nullable=True)
+    is_completed = db.Column(db.Boolean, default=False)
+    failed_attempts = db.Column(db.Integer, nullable=False, default=0)
+
+
+class SentenceToList(db.Model):
+    sentence_id = db.Column(db.Integer, db.ForeignKey(SENTENCE_REFERENCE), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(LIST_REFERENCE), nullable=False)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint(sentence_id, list_id),
+        {}
+    )
+
+
+migrate = Migrate(app, db)
 
 # when we reach here, we should already have an initialized database layer
 """ROUTES AND HANDLERS"""
