5b1e1baad746ec634117118657f4ea21fa6fd6d1
mtalimanchuk <mtalimanchuk@gmail.com>
2019-08-15 23:54:40 +0300

    fix log files extension

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -15,7 +15,7 @@ timeout = 120
 #
 
 _logpath = 'gunicorn_logs/'
-errorlog = _logpath + 'errors'
+errorlog = _logpath + 'errors.log'
 loglevel = 'info'
-accesslog = _logpath + 'access'
+accesslog = _logpath + 'access.log'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
