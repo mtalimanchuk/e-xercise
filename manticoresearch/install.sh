fc0a99831327aeb89cde7dfa3b129478bbc764de
mtalimanchuk <mtalimanchuk@gmail.com>
2019-08-15 23:48:56 +0300

    cleanup

--- manticoresearch/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://github.com/manticoresoftware/manticore/releases/download/2.4.1/manticore_2.4.1-171017-3b31a97-release-stemmer.jessie_amd64-bin.deb &&
-dpkg -i manticore_2.4.1-171017-3b31a97-release-stemmer.jessie_amd64-bin.deb
