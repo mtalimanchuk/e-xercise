e4df6c3006917c2b9cdd34e5046e70c5994f9b04
mtalimanchuk <mtalimanchuk@gmail.com>
2019-08-22 02:23:54 +0300

    cleanup

--- config.py
+++ config.py
@@ -5,15 +5,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "suchsafemuchsecret"
-    # DB
-    DB_HOST = 'localhost'
-    DB_USERNAME = 'classroom'
-    DB_PASSWORD = 'classroom'
-    DB_NAME = 'classroom_db'
-
-    DB_URL = os.environ.get('DATABASE_URL') or \
-             'mysql://' + DB_USERNAME + ":" + DB_PASSWORD + "@" + DB_HOST + ':3306/' + DB_NAME
-    SQLALCHEMY_DATABASE_URI = DB_URL
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # ETC
-    # DEBUG = True
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -20,5 +20,5 @@ loglevel = "info"
 accesslog = _logpath + "access.log"
 # https://stackoverflow.com/a/25737799/4443571
 access_log_format = """
-    %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s" "%({Header}i)s"
+    %(h)s %({X-Real-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"
 """
