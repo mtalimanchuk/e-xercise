340a827e4ec0b30e240894f03d0323c38f685377
mtalimanchuk <mtalimanchuk@gmail.com>
2019-09-24 00:54:30 +0300

    fix exercise submission not working on enter key

--- app/static/javascript/exercise.js
+++ app/static/javascript/exercise.js
@@ -1,9 +1,10 @@
-function checkTaskSubmit(event) {
+function checkTaskSubmit(url, event) {
   event.preventDefault();
   let taskId = event.target.getAttribute("task_id");
   let taskAnswer = document.getElementById(taskId).value;
   let payload = {"task_id": taskId, "task_answer": taskAnswer};
-  fetch({{ url_for('check')|tojson }}, {
+  
+  fetch(url, {
     method: 'POST',
     body: JSON.stringify(payload)
   })
@@ -23,7 +24,3 @@ function showTaskSubmitResult(data) {
     input.style.backgroundColor = '#ff939c';
   }
 }
-let submitButtonsArray = document.getElementsByName("check-task");
-submitButtonsArray.forEach(function(element) {
-    element.addEventListener('click', checkTaskSubmit)
-})
